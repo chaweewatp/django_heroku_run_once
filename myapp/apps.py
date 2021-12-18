50e4bc6d2d0073bf2402768bdd9daa18f45644ea
pornchai chaweewat <backendteam.pea@gmail.com>
2021-12-18 21:57:45 +0700

    third solution edited

--- myapp/apps.py
+++ myapp/apps.py
@@ -42,37 +42,40 @@ import os
 from django.apps import AppConfig
 import psutil
 
-def ready(self):
-    startScheduler = True
+class MyappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myapp'
+    def ready(self):
+        startScheduler = True
 
-    #check WEB_CONCURRENCY exists and is more than 1
-    web_concurrency = os.environ.get("WEB_CONCURRENCY")
-    if (web_concurrency):
-        mypid = os.getpid()
-        print("[%s] WEB_CONCURRENCY exists and is set to %s" % (mypid, web_concurrency))
-        gunicorn_workers = int(web_concurrency)
-        if (gunicorn_workers > 1):
-            maxPid = self.getMaxRunningGunicornPid()
-            if (maxPid == mypid):
-                startScheduler = True
-            else:
-                startScheduler = False
+        #check WEB_CONCURRENCY exists and is more than 1
+        web_concurrency = os.environ.get("WEB_CONCURRENCY")
+        if (web_concurrency):
+            mypid = os.getpid()
+            print("[%s] WEB_CONCURRENCY exists and is set to %s" % (mypid, web_concurrency))
+            gunicorn_workers = int(web_concurrency)
+            if (gunicorn_workers > 1):
+                maxPid = self.getMaxRunningGunicornPid()
+                if (maxPid == mypid):
+                    startScheduler = True
+                else:
+                    startScheduler = False
 
-    if (startScheduler):
-        print("[%s] WILL START SCHEDULER", mypid)
-    else:
-        print("[%s] WILL NOT START SCHEDULER", mypid)
+        if (startScheduler):
+            print("[%s] WILL START SCHEDULER", mypid)
+        else:
+            print("[%s] WILL NOT START SCHEDULER", mypid)
 
-def getMaxRunningGunicornPid(self):
-    running_pids = psutil.pids()
-    maxPid = -1
-    for pid in running_pids:
-        proc = psutil.Process(pid)
-        proc_name = proc.name()
-        if (proc_name == "gunicorn"):
-            if (maxPid < pid):
-                maxPid = pid
-    print("Max Gunicorn PID: %s", maxPid)
-    return maxPid
+    def getMaxRunningGunicornPid(self):
+        running_pids = psutil.pids()
+        maxPid = -1
+        for pid in running_pids:
+            proc = psutil.Process(pid)
+            proc_name = proc.name()
+            if (proc_name == "gunicorn"):
+                if (maxPid < pid):
+                    maxPid = pid
+        print("Max Gunicorn PID: %s", maxPid)
+        return maxPid
 
 
